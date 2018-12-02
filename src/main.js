ae8dd27e47b20a7f061dce3928a3bf20573a3ef2
hckang <hckang80@gmail.com>
2018-12-02 17:28:02 +0900

    Change to rotate the minute hand and hour hand in seconds

--- src/main.js
+++ src/main.js
@@ -3,15 +3,21 @@ const clockBody = document.querySelector('.clock-wrap .body')
 const theHourHand = document.querySelector('.the-hour-hand')
 const theMinuteHand = document.querySelector('.the-minute-hand')
 const theSecondHand = document.querySelector('.the-second-hand')
+const centeringHands = 'translateX(-50%)'
 
 const handMovement = () => {
   const date = new Date()
   const hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours()
   const minutes = date.getMinutes()
   const seconds = date.getSeconds()
-  theHourHand.style.transform = `rotate(${hours * 30}deg) translateX(-50%)`
-  theMinuteHand.style.transform = `rotate(${minutes * 6}deg) translateX(-50%)`
-  theSecondHand.style.transform = `rotate(${seconds * 6}deg) translateX(-50%)`
+  const rotationValue = {
+    hours: hours * 30 + minutes * .5 + seconds * 1 / 120,
+    minutes: minutes * 6 + seconds * .1,
+    seconds: seconds * 6
+  }
+  theHourHand.style.transform = `rotate(${rotationValue.hours}deg) ${centeringHands}`
+  theMinuteHand.style.transform = `rotate(${rotationValue.minutes}deg) ${centeringHands}`
+  theSecondHand.style.transform = `rotate(${rotationValue.seconds}deg) ${centeringHands}`
 }
 
 handMovement()
